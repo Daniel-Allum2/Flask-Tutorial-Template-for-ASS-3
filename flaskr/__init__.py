c1bc034bc958086e63ef7a5e17b504c92d026c7d
Daniel-Allum2 <danielallum@icloud.com>
2026-05-28 00:38:40 +0000

    css and html templates

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,12 +11,10 @@ def create_app(test_config=None):
     from . import db
 
     db.init_app(app)
-    return app
 
     from . import auth
 
     app.register_blueprint(auth.bp)
-    return app
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
